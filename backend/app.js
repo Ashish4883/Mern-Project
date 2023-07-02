f0d8f9b5a70c6c83e3bc6aef5785f887a492a75b
Ashish4883 <aashishmishra0016@gmail.com>
2023-07-02 13:10:11 +0530

    added mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://Ashish:K86t4JRRRIFNo6Xz@mern-project.oparxc3.mongodb.net/node-angular?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to Database!");
+  })
+  .catch(() => {
+    console.log("Connection Failed!");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,44 +34,31 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: "Post Added Successfully",
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post Added Successfully",
+      postId : createdPost._id
+    });
   });
 });
 
-app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fafafafh0001",
-      title: "First Post Server",
-      content: "First Post Content coming from server",
-    },
-    {
-      id: "fafafafh0002",
-      title: "Second Post Server",
-      content: "Second Post Content coming from server",
-    },
-    {
-      id: "fafafafh0003",
-      title: "Third Post Server",
-      content: "Third Post Content coming from server",
-    },
-    {
-      id: "fafafafh0004",
-      title: "Fourth Post Server",
-      content: "Fourth Post Content coming from server",
-    },
-    {
-      id: "fafafafh0005",
-      title: "Fifth Post Server",
-      content: "Fifth Post Content coming from server",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts,
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents,
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
   });
 });
 
